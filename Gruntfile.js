afe6176fe3838d2ffd26f4ec68e3cfa05acc0ccf
Tomasz Szymczakowski <tomasz.szymczakowski@cybercom.com>
2016-03-17 10:36:52 +0100

    Clean and create distribution dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,59 @@
-module.exports = function(grunt) {
-	grunt.registerTask('help', 'displays console help', function(){
-		grunt.log.writeln('Welcome to grunt help...');
-		grunt.log.subhead('Oko');
-	});
-  grunt.initConfig({
-    connect: {
-		server: {
-			options: {
-				port: 9000,
-				hostname: 'localhost',
-				open: true,
-				livereload: true
-			}
-		}
-    },
-	watch: {
-		options: {
-			livereload: true
-		},
-		livereload:{
-			files: 'index.html',
-			tasks: []
-		}
-	}
-  });
-	require('load-grunt-tasks')(grunt);
+module.exports = function (grunt) {
+    grunt.registerTask('help', 'displays console help', function () {
+        grunt.log.writeln('Welcome to grunt help...');
+        grunt.log.subhead('Oko');
+    });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cfg: {
+            paths: {
+                root: __dirname,
+                source: '<%= cfg.paths.root %>/src',
+                output: '<%= cfg.paths.root %>/dist',
+                bower: '<%= cfg.paths.root %>/bower_components'
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    base: 'src/',
+                    port: 9000,
+                    hostname: 'localhost',
+                    open: true,
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            livereload: {
+                files: '<%= cfg.paths.output %>/index.html',
+                tasks: []
+            }
+        },
+        clean: {
+            dist: ['<%= cfg.paths.output %>']
+        },
+        copy: {
+            html: {
+                src: '<%= cfg.paths.source %>/index.html',
+                dest: '<%= cfg.paths.output %>/index.html'
+            },
+            bootstrap: {
+                src: '<%= cfg.paths.bower %>/bootstrap/dist/css/bootstrap.min.css',
+                dest: '<%= cfg.paths.output %>/bootstrap.min.css'
+            },
+            jumbotron: {
+                src: '<%= cfg.paths.source %>/styles/jumbotron.css',
+                dest: '<%= cfg.paths.output %>/jumbotron.css'
+            }
+        }
+    });
+    require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('index', ['connect']);
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['index', 'watch']);
+    grunt.registerTask('index', ['connect']);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['clean', 'copy', 'index', 'watch']);
 };
